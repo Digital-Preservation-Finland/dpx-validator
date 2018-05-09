919412941374db1982e92952a4fa99a690733da8
Aleksi Salo <aleksi.salo@csc.fi>
2018-05-09 15:56:41 +0300

    Added setup.py with setuptools installation

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(name="dpx_validator",
+      version="0.1",
+      packages=find_packages(),
+      scripts=["dpx_validator/dpx_validator.py"]
+)
