3a184523abee98ddee2d7cfba1c3f94c412093b2
Aleksi Salo <aleksi.salo@csc.fi>
2018-05-15 16:23:25 +0300

    KDKPAS-1638 Added docstrings

--- dpxv/validations.py
+++ dpxv/validations.py
@@ -1,16 +1,50 @@
-"""VALIDATION PROCEDURES
-List of functions used to validate various fields in a header. The functions get field variable which is bytearray representation of read section from the file"""
+"""Validation procedures
+
+Functions defined in this file are used to validate
+various fields in a header.
+
+Functions get in 'field' variable data from a section
+in header for validation. Any other variables are
+defined in **kwargs and are shared by every function
+The section and the validation function are defined,
+connected, on 'Field' class.
+
+"""
 
 import os
-from struct import unpack
+from struct import unpack, calcsize
 
 from .models import ValidationError
 
 
-BYTEORDER = ">" # Bigendian
+# Bigendian byte order by default
+BYTEORDER = ">"
+
+
+def read_field(f, field):
+    """The byte reading procedure for a section in a file"""
+
+    f.seek(field.offset)
+    length = calcsize(field.pformat)
+
+    a = bytearray(f.read(length))
+
+    global BYTEORDER
+    data = unpack(BYTEORDER+field.pformat, a)
+
+    if len(data) == 1:
+        return data[0]
+
+    return data
 
 
 def check_magic_number(field, **kwargs):
+    """Magic number should be 'SPDX' or reversed 'XDPS'.
+
+    As this is the first validation procedure, if validation fails
+    attempt byte order flip on the fly to continue process.
+
+    """
 
     global BYTEORDER
     validate_by = unpack(BYTEORDER+'I', 'SDPX')[0]
@@ -22,20 +56,26 @@ def check_magic_number(field, **kwargs):
         if not field == validate_by:
             raise ValidationError('Invalid magic number: %s' % field)
 
+        # Byte order and bit order are not the same
         print 'Byte order changed to littleendian'
-        #Byte order and bit order are not the same
         BYTEORDER = '<'
 
 
 def offset_to_image(field, **kwargs):
+    """Offset to image data defined in header should
+    not be greater than actual size of the file.
+
+    """
 
     size = os.stat(kwargs['path']).st_size
 
     if field > size:
-        raise ValidationError('Offset to image %s is more file size %s ' % (field, size))
+        raise ValidationError(
+            'Offset to image %s is more file size %s ' % (field, size))
 
 
 def check_version(field, **kwargs):
+    """DPX version should be 'V2.0'."""
 
     field = bytearray(field).rsplit('\0')[0]
 
@@ -44,14 +84,22 @@ def check_version(field, **kwargs):
 
 
 def check_filesize(field, **kwargs):
+    """File size defined in header should match to what
+    for example file system says.
+
+    """
 
     size = os.stat(kwargs['path']).st_size
 
     if not field == size:
-        raise ValidationError("File size in header (%s) differs from filesystem size %s" % (str(field), size))
+        raise ValidationError(
+            "File size in header (%s) differs"
+            "from filesystem size %s" % (str(field), size))
 
 
 def check_unencrypted(field, **kwargs):
+    """DPX files should be unecrypted."""
 
-    if not 'fffffff' in hex(field):
-        raise ValidationError("Encryption field in header not NULL or undefined")
+    if 'fffffff' not in hex(field):
+        raise ValidationError(
+            "Encryption field in header not NULL or undefined")
