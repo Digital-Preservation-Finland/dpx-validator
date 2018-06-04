cae9a06804b964a29e5da72c91a493c89af22b42
Juha Lehtonen <juha.lehtonen@csc.fi>
2018-06-04 15:46:33 +0300

    KDKPAS-1724 Version 0.3.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="dpx-validator",
-      version="0.2",
+      version="0.3",
       packages=find_packages(exclude=['tests']),
       entry_points={'console_scripts':
           ["dpxv=dpx_validator.dpxv:main"]}
