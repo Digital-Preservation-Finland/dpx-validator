2500ff431d37eaa501b3974f7719b0f5fcb984ba
Juha Lehtonen <juha.lehtonen@csc.fi>
2018-07-27 18:24:21 +0300

    KDKPAS-1751 Add version.py

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages
 
+from version import get_version
+
 setup(name="dpx-validator",
-      version="0.3",
+      version=get_version(),
       packages=find_packages(exclude=['tests']),
       entry_points={'console_scripts':
           ["dpxv=dpx_validator.dpxv:main"]}
