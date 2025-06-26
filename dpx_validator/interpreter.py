94e110bf943595e81421afb7729e3b05711f1b26
Henri Johansson <hjohanss@csc.fi>
2025-06-26 13:15:23 +0000

    TPASPKT-1502 Add interpreter.py

---
+++ dpx_validator/interpreter.py
@@ -0,0 +1,34 @@
+from struct import unpack, calcsize
+
+
+class FileHeaderReader:
+    """
+    Reads the file
+    """
+
+    # Bigendian byte order by default for struct.unpack
+    byte_order = ">"
+
+    @staticmethod
+    def littleendian_byteorder(self):
+        """Change byte order interpretation to littleendian"""
+        FileHeaderReader.byte_order = "<"
+
+    @staticmethod
+    def read_field(self, file_handle, header):
+        """Extract header field value.
+
+        :file_handle: `file` handle opened for reading
+        :field: Item from `dpx_validator.api.VALIDATED_FIELDS`"""
+
+        length = calcsize(header["data_form"])
+
+        file_handle.seek(header["offset"])
+        data = file_handle.read(length)
+
+        unpacked = unpack(self.byte_order+header["data_form"], data)
+
+        if len(unpacked) == 1:
+            return unpacked[0]
+
+        return unpacked
