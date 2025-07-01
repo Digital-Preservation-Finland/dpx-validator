a0e6455b411b013c159dd43428ba65f592edd5ee
Henri Johansson <hjohanss@csc.fi>
2025-07-01 10:26:31 +0000

    TPASPKT-1502 Refactor and renamed dpxv_test.py to match new module name main.py

--- tests/main_test.py
+++ tests/main_test.py
@@ -4,7 +4,7 @@ from subprocess import STDOUT, call, check_output
 
 import pytest
 
-from dpx_validator.dpxv import main
+from dpx_validator.main import main
 
 
 @pytest.mark.parametrize("testfile,returncode", [
@@ -18,7 +18,7 @@ def test_returncode(testfile, returncode):
     """Test with valid, corrupted and empty files."""
 
     code = call(
-        ['python3', '-m', 'dpx_validator.dpxv', testfile],
+        ['python3', '-m', 'dpx_validator.main', testfile],
         env={'PYTHONPATH': '.'})
 
     assert code == returncode
@@ -29,7 +29,7 @@ def test_byteorder_switch(littleendian_file):
     big endian is default."""
 
     output = check_output(
-        ['python3', '-m', 'dpx_validator.dpxv', littleendian_file.strpath],
+        ['python3', '-m', 'dpx_validator.main', littleendian_file.strpath],
         env={'PYTHONPATH': '.'})
 
     output = str(output, "utf-8")
@@ -42,7 +42,7 @@ def test_empty_file():
     empty_file = 'tests/data/empty_file.dpx'
 
     output = check_output(
-        ['python3', '-m', 'dpx_validator.dpxv', empty_file],
+        ['python3', '-m', 'dpx_validator.main', empty_file],
         env={'PYTHONPATH': '.'}, stderr=STDOUT)
 
     output = str(output, "utf-8")
@@ -56,7 +56,7 @@ def test_partial_file(test_file):
     partial_file = test_file.strpath
 
     output = check_output(
-        ['python3', '-m', 'dpx_validator.dpxv', partial_file],
+        ['python3', '-m', 'dpx_validator.main', partial_file],
         env={'PYTHONPATH': '.'}, stderr=STDOUT)
 
     output = str(output, "utf-8")
