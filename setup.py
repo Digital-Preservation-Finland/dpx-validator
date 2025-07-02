f7e31c7b1349dd3f81b5d0615d965fb0dd8ae7ec
Henri Johansson <hjohanss@csc.fi>
2025-07-02 07:55:09 +0000

    TPASPKT-1502 Change execution location to main

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     entry_points={
-        'console_scripts': ["dpxv=dpx_validator.dpxv:main"]
+        'console_scripts': ["dpxv=dpx_validator.main:main"]
     }
 )
